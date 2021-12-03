257a973452cf7bd835047a7a93cd05c688130153
Oyebola Simeon Oyekunle <simeonoyekunleoyebola@gmail.com>
2021-12-03 03:26:15 +0100

    update JS
    - Add date and time to todo app
    - work on local storage to cache browser data
    - redeploy app on netlify via Git

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
 const addTask = () =>{
     let resultDiv = document.getElementById("results");
     let inputText = document.querySelector("input");
-    let addTodo = document.getElementById("addMe");
+  //  let addTodo = document.getElementById("addMe");
 
     console.log(inputText.value);
     var fontAwe = document.createElement('span');
 
+    //Date and Time
+
+    let d = new Date();
+    currentDate = " "+d.getFullYear()+"-"+d.getMonth()+"-"+d.getDay()+" "+d.getHours()+":"+d.getMinutes()+":"+d.getSeconds();
+    console.log(currentDate)
+
         var linePara = document.createElement('p');
+        var dt = document.createElement('i')
+        dt.innerText = currentDate;
+        dt.style.paddingLeft = "20px"
+        dt.style.fontSize = "16px"
+        //console.log(dt.innerText)
         linePara.innerText = inputText.value
+
+       // var sim = [];
         if(linePara.innerText === ""){
             alert("cannot insert empty task")
         }else{
@@ -16,8 +29,40 @@ const addTask = () =>{
             fontAwe.classList.add('fa-window-close')
             fontAwe.classList.add('fon')
             linePara.classList.add('nike');
+            linePara.appendChild(dt);
             resultDiv.appendChild(linePara);
             linePara.appendChild(fontAwe)
+
+            var htmlContents = document.body.inn
+            localStorage.setItem('myBook', JSON.stringify(htmlContents ));
+
+            
+        //     let initstoreMemory = linePara.innerText;
+
+        //     localStorage.setItem('store', initstoreMemory)
+        //     let storeMemory = localStorage.store;
+        //    // sim.push(linePara)
+        //     console.log(storeMemory);
+            
+        //     console.log(sim) currentDate and InputText
+            
+            // resultDiv.append(storeMemory)
+
+            // console.log(typeof(linePara))
+
+            // var lineParas = JSON.stringify(resultDiv.innertext)
+            // console.log(lineParas)
+            if(inputText){
+                localStorage.setItem('keys', inputText)
+                localStorage.setItem('dates', currentDate)
+            
+                
+            }
+
+            for(let i = 0; i < localStorage.length; i++){
+                const value = localStorage.keys
+                const data = localStorage.dates
+            }
 
             inputText.value = "";
 
